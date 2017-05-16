ba0a356e70e13579cef31ea234ec38faf6b6375a
Katrix- <katrix97@hotmail.com>
2017-05-16 19:22:44 +0200

    Deprecate InitNeeded

--- shared/src/main/scala/io/github/katrix/katlib/InitNeeded.scala
+++ shared/src/main/scala/io/github/katrix/katlib/InitNeeded.scala
@@ -25,6 +25,8 @@ import io.github.katrix.katlib.helper.LogHelper
 /**
 	* A small trait to easily load some class at some later point
 	*/
+@deprecated(since = "KatLib 2.2")
+@deprecatedInheritance(since = "KatLib 2.2")
 trait InitNeeded {
 
   def init()(implicit plugin: KatPlugin): Unit = LogHelper.trace(s"Loaded ${getClass.getName}")
