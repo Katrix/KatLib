29ee0b6ea3eab95ca24401ec0ccf1f31b89d3dfb
Katrix- <katrix97@hotmail.com>
2017-07-23 20:50:04 +0200

    More work on i18n
    Fix resources file
    Make it possible to use Text as parameters

--- shared/src/main/scala/io/github/katrix/katlib/i18n/Resource.scala
+++ shared/src/main/scala/io/github/katrix/katlib/i18n/Resource.scala
@@ -3,11 +3,14 @@ package io.github.katrix.katlib.i18n
 import java.util.{Locale, ResourceBundle}
 
 import scala.collection.JavaConverters._
+import scala.util.matching.Regex
 
 import org.apache.commons.lang3.text.StrSubstitutor
-import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.{Text, TextTemplate}
 
 trait Resource {
+  val findArgs: Regex = """\$\{(.+?)\}""".r
+
   def getBundle(implicit locale: Locale): ResourceBundle
 
   def get(key: String)(implicit locale: Locale): String = getBundle.getString(key)
@@ -17,9 +20,24 @@ trait Resource {
 
   def getText(key: String)(implicit locale: Locale): Text = Text.of(get(key))
 
-  def getText(key: String, params: Map[String, String])(implicit locale: Locale): Text = Text.of(get(key, params))
-  def getText(key: String, params: (String, String)*)(implicit locale: Locale):   Text = Text.of(get(key, params.toMap))
+  def getText(key: String, params: Map[String, _])(implicit locale: Locale): Text = formatText(get(key), params)
+  def getText(key: String, params: (String, _)*)(implicit locale: Locale):   Text = getText(key, params.toMap)
 
-  protected def format(str: String, params: Map[String, String]): String =
+  private def format(str: String, params: Map[String, String]): String =
     new StrSubstitutor(params.asJava).replace(str)
+
+  private def formatText(str: String, params: Map[String, _]): Text = {
+    if(params.forall(t => t._2.isInstanceOf[String])) Text.of(format(str, params.asInstanceOf[Map[String, String]]))
+    else {
+      val args = findArgs.findAllIn(str).toSeq.map(s => TextTemplate.arg(s.substring(2, s.length - 1)))
+      val nonArgs = findArgs.split(str).toSeq.padTo(args.length, "")
+
+      val objs = nonArgs.zip(args).flatMap { case (str1, str2) =>
+        if(str1.isEmpty) Seq(str2)
+        else Seq(str1, str2)
+      }
+
+      TextTemplate.of("${", "}", objs: _*).apply(params.asJava).build()
+    }
+  }
 }
