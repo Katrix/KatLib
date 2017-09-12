7ab34a51072395bee2d62f58ef89b752d37838db
Unknown <katrix97@hotmail.com>
2017-09-12 21:28:15 +0200

    Fix compile error in checkSpongeVersion

--- shared/src/main/scala/io/github/katrix/katlib/KatLibBase.scala
+++ shared/src/main/scala/io/github/katrix/katlib/KatLibBase.scala
@@ -12,12 +12,14 @@ trait KatLibBase extends KatPlugin {
 
   private val SemVer = """(\d+)\.(\d+)\.(\d+).*""".r
 
-  def checkSpongeVersion(spongeVersionOpt: Option[String], expectedVersion: String): Unit = {
+  def checkSpongeVersion(spongeVersionOpt: Option[String], expectedVersionStr: String): Unit = {
+    implicit val plugin: KatPlugin = this
+
     spongeVersionOpt match {
       case Some(spongeVersion) =>
         spongeVersion match {
           case SemVer(majorStr, minorStr, _) =>
-            expectedVersion match {
+            expectedVersionStr match {
               case SemVer(expectedMajorStr, expectedMinorStr, _) =>
                 val major         = majorStr.toInt
                 val expectedMajor = expectedMajorStr.toInt
@@ -26,13 +28,13 @@ trait KatLibBase extends KatPlugin {
                 val expectedMinor = expectedMinorStr.toInt
 
                 if (major != expectedMajor)
-                  LogHelper.warn(
-                    s"This version of KatLib is not compiled against Sponge API $major.x.x. Stuff might and probably will break"
-                  )
+                  LogHelper.warn(s"""This version of KatLib is not compiled against Sponge API $major.x.x. 
+                       |Stuff might and probably will break""".stripMargin)
 
                 if (minor < expectedMinor)
                   LogHelper.warn(
-                    s"This version of KatLib is compiled against $expectedMajor.$expectedMinor.x, but $major.$minor.x was found. Stuff might not work as expected. Consider upgrading your version of Sponge"
+                    s"""This version of KatLib is compiled against $expectedMajor.$expectedMinor.x, but $major.$minor.x was found. 
+                       |Stuff might not work as expected. Consider upgrading your version of Sponge""".stripMargin
                   )
 
               case _ => LogHelper.warn("This version of KatLib was compiled for an unrecognized version")
