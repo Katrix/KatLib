70f089e543690d9242cbd948d84339a19460b9e5
Unknown <katrix97@hotmail.com>
2018-03-16 04:04:57 +0100

    Update scammander

--- build.sbt
+++ build.sbt
@@ -9,28 +9,6 @@ lazy val katLib = crossProject(SpongePlatform("5.0.0"), SpongePlatform("6.0.0"),
     organization := "net.katsstuff",
     version := "3.0.0-SNAPSHOT",
     scalaVersion := "2.12.4",
-    assemblyShadeRules in assembly := Seq(
-      ShadeRule.rename("cats.**"                    -> "io.github.katrix.homesweethome.shade.cats.@1").inAll,
-      ShadeRule.rename("com.google.protobuf.**"     -> "io.github.katrix.homesweethome.shade.protobuf.@1").inAll,
-      ShadeRule.rename("com.trueaccord.lenses.**"   -> "io.github.katrix.homesweethome.shade.lenses.@1").inAll,
-      ShadeRule.rename("com.trueaccord.scalapb.**"  -> "io.github.katrix.homesweethome.shade.scalapb.@1").inAll,
-      ShadeRule.rename("fansi.**"                   -> "io.github.katrix.homesweethome.shade.fansi.@1").inAll,
-      ShadeRule.rename("fastparse.**"               -> "io.github.katrix.homesweethome.shade.fastparse.@1").inAll,
-      ShadeRule.rename("io.circe.**"                -> "io.github.katrix.homesweethome.shade.circe.@1").inAll,
-      ShadeRule.rename("jawn.**"                    -> "io.github.katrix.homesweethome.shade.jawn.@1").inAll,
-      ShadeRule.rename("machinist.**"               -> "io.github.katrix.homesweethome.shade.machinist.@1").inAll, //Zap?
-      ShadeRule.rename("metaconfig.**"              -> "io.github.katrix.homesweethome.shade.metaconfig.@1").inAll,
-      ShadeRule.rename("org.langmeta.**"            -> "io.github.katrix.homesweethome.shade.langmeta.@1").inAll,
-      ShadeRule.rename("org.scalameta.**"           -> "io.github.katrix.homesweethome.shade.scalameta.@1").inAll,
-      ShadeRule.rename("org.typelevel.paiges.**"    -> "io.github.katrix.homesweethome.shade.paiges.@1").inAll,
-      ShadeRule.rename("pprint.**"                  -> "io.github.katrix.homesweethome.shade.pprint.@1").inAll,
-      ShadeRule.rename("scala.**"                   -> "io.github.katrix.homesweethome.shade.scala.@1").inAll,
-      ShadeRule.rename("scalapb.**"                 -> "io.github.katrix.homesweethome.shade.scalapb.@1").inAll,
-      ShadeRule.rename("shapeless.**"               -> "io.github.katrix.homesweethome.shade.shapeless.@1").inAll,
-      ShadeRule.rename("sourcecode.**"              -> "io.github.katrix.homesweethome.shade.sourcecode.@1").inAll,
-      ShadeRule.rename("io.github.katrix.katlib.**" -> "io.github.katrix.homesweethome.shade.katlib.@1").inAll,
-      ShadeRule.zap("macrocompat.**").inAll
-    ),
     scalacOptions ++= Seq(
       "-deprecation",
       "-feature",
@@ -49,7 +27,7 @@ lazy val katLib = crossProject(SpongePlatform("5.0.0"), SpongePlatform("6.0.0"),
       .map(_ % circeVersion),
     libraryDependencies += "com.geirsson"  %% "metaconfig-core"            % "0.6.0",
     libraryDependencies += "com.geirsson"  %% "metaconfig-typesafe-config" % "0.6.0",
-    libraryDependencies += "net.katsstuff" %% "scammander-sponge7"         % "0.2",
+    libraryDependencies += "net.katsstuff" %% "scammander-sponge7"         % "0.3-SNAPSHOT",
     //https://github.com/portable-scala/sbt-crossproject/issues/74
     Seq(Compile, Test).flatMap(inConfig(_) {
       unmanagedResourceDirectories ++= {
---
+++ katLib/src/main/scala/io/github/katrix/katlib/command/KatLibCommands.scala
@@ -0,0 +1,132 @@
+package io.github.katrix.katlib.command
+
+import org.spongepowered.api.command.CommandSource
+import org.spongepowered.api.service.pagination.PaginationList
+import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.action.TextActions
+
+import io.github.katrix.katlib.KatPlugin
+import net.katsstuff.scammander.sponge._
+import io.github.katrix.katlib.helper.Implicits._
+import org.spongepowered.api.text.format.TextColors._
+import org.spongepowered.api.text.format.TextStyles._
+
+trait KatLibCommands extends SpongeBaseAll {
+
+  implicit def plugin: KatPlugin
+
+  override type Title = Text
+
+  private val Branch = "├─"
+  private val Line   = "│"
+  private val End    = "└─"
+
+  override def sendMultipleCommandHelp(
+      title: Text,
+      source: CommandSource,
+      commands: Set[ChildCommand[_, _]]
+  ): CommandStep[CommandSuccess] = {
+    val pages = PaginationList.builder()
+
+    val helpTexts = commands.toSeq
+      .filter(_.command.testPermission(source))
+      .sortBy(_.aliases.head)
+      .flatMap { child =>
+        createCommandHelp(
+          source,
+          child.aliases.mkString("/", "|", ""),
+          s"/${child.aliases.head}",
+          child.command,
+          detail = false
+        )
+      }
+
+    pages.title(title).contents(helpTexts: _*).sendTo(source)
+    Command.successStep()
+  }
+
+  override def sendCommandHelp(
+      title: Text,
+      source: CommandSource,
+      command: StaticChildCommand[_, _],
+      path: List[String]
+  ): CommandStep[CommandSuccess] = {
+    if (command.testPermission(source)) {
+      val commandName = path.mkString("/", " ", "")
+      val pages       = PaginationList.builder()
+      pages
+        .title(title)
+        .contents(createCommandHelp(source, commandName, commandName, command, detail = true): _*)
+        .sendTo(source)
+      Command.successStep()
+    } else Command.errorStep("You don't have the permission to see the help for this command")
+  }
+
+  def createCommandHelp(
+      source: CommandSource,
+      commandName: String,
+      fullCommandName: String,
+      command: StaticChildCommand[_, _],
+      detail: Boolean,
+      indent: Int = 0,
+      isIndentEnd: Boolean = false
+  ): Seq[Text] = {
+    val usage = command.getUsage(source)
+
+    val helpBase =
+      t"$GREEN$UNDERLINE$commandName $usage".toBuilder.onClick(TextActions.suggestCommand(fullCommandName)).build()
+
+    val commandHelp        = command.getHelp(source).toOption
+    val commandDescription = command.getShortDescription(source).toOption
+
+    val withHover =
+      commandDescription.fold(helpBase)(desc => helpBase.toBuilder.onHover(TextActions.showText(t"$desc")).build())
+
+    val withExtra = if (detail) {
+      commandHelp.orElse(commandDescription).fold(withHover)(desc => t"$withHover - $desc")
+    } else {
+      commandDescription.fold(withHover)(desc => t"$withHover - $desc")
+    }
+
+    val children = command.command.children.toSeq.sortBy(_.aliases.head)
+    val childHelp = if (children.nonEmpty) {
+
+      val childrenTopHelp = children.init.flatMap {
+        case ChildCommand(aliases, childCommand) =>
+          createCommandHelp(
+            source,
+            aliases.mkString("|"),
+            s"$fullCommandName ${aliases.head}",
+            childCommand,
+            detail = false,
+            indent = indent + 1
+          )
+      }
+      val lastChild = children.last
+      val lastChildHelp = createCommandHelp(
+        source,
+        lastChild.aliases.mkString("|"),
+        s"$fullCommandName ${lastChild.aliases.head}",
+        lastChild.command,
+        detail = false,
+        indent = indent + 1,
+        isIndentEnd = true
+      )
+      childrenTopHelp ++ lastChildHelp
+    } else Nil
+
+    if (indent == 1) {
+      val piece    = if (isIndentEnd) End else Branch
+      val indented = t"$piece $withExtra"
+
+      indented +: childHelp
+    } else if (indent != 0) {
+      val end      = if (isIndentEnd) End else Branch
+      val spaces   = (indent - 1) * 2
+      val space    = " " * spaces
+      val indented = t"$Line$space$end $withExtra"
+
+      indented +: childHelp
+    } else withExtra +: childHelp
+  }
+}
